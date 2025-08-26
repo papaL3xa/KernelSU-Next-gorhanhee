8d607492e16253c2b57ba4a33817f45c2312c112
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-08-26 20:38:27 +0800

    kernel: selinux: fix pointer mismatch with 32-bit ksud on 64-bit kernels
    Since KernelSU Manager can now be built for 32-bit, theres this problematic
    setup where userspace is 32-bit (armeabi-v7a) and kernel is 64bit (aarch64).
    
    On 64-bit kernels with CONFIG_COMPAT=y, 32-bit userspace passes 32-bit pointers.
    These values are interpreted as 64-bit pointers without proper casting and that
    results in invalid or near-null memory access.
    
    This patch adds proper compat-mode handling with the ff changes:
    - introduce a dedicated struct (`sepol_compat_data`) using u32 fields
    - use `compat_ptr()` to safely convert 32-bit user pointers to kernel pointers
    - adding a runtime `ksu_is_compat` flag to dynamically select between struct layouts
    
    This prevents a near-null pointer dereference when handling SELinux
    policy updates from 32-bit ksud in a 64-bit kernel.
    
    Truth table:
    
    kernel 32 + ksud 32, struct is u32, no compat_ptr
    kernel 64 + ksud 32, struct is u32, yes compat_ptr
    kernel 64 + ksud 64, struct is u64, no compat_ptr
    
    Preprocessor check
    
    64BIT=y COMPAT=y: define both structs, select dynamically
    64BIT=y COMPAT=n: struct u64
    64BIT=n: struct u32
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/core_hook.c
+++ kernel/core_hook.c
@@ -1477,20 +1477,6 @@ LSM_HANDLER_TYPE ksu_bprm_check(struct linux_binprm *bprm)
 }
 
 #ifdef CONFIG_KSU_LSM_SECURITY_HOOKS
-
-int ksu_bprm_check(struct linux_binprm *bprm)
-{
-	char *filename = (char *)bprm->filename;
-	
-	if (likely(!ksu_execveat_hook))
-		return 0;
-
-	ksu_handle_pre_ksud(filename);
-
-	return 0;
-
-}
-
 static int ksu_task_prctl(int option, unsigned long arg2, unsigned long arg3,
 			  unsigned long arg4, unsigned long arg5)
 {
