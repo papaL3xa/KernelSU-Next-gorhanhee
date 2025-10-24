43fddff70934413885a53fc10758e53885f333b4
papal3xa <john.scriven03@gmail.com>
2025-10-24 10:20:52 +0700

    Update setup.sh

--- kernel/setup.sh
+++ kernel/setup.sh
@@ -42,15 +42,11 @@ setup_kernelsu() {
     test -d "$GKI_ROOT/KernelSU-Next" || git clone git@github.com:papaL3xa/KernelSU-Next-gorhanhee.git KernelSU-Next && echo "[+] Repository cloned."
     cd "$GKI_ROOT/KernelSU-Next"
     git stash && echo "[-] Stashed current changes."
-    if [ "$(git status | grep -Po 'v\d+(\.\d+)*' | head -n1)" ]; then
-        git checkout KSUNOG && echo "[-] Switched to KSUNOG branch."
-    fi
+    
+    git checkout KSUNOG && echo "[-] Switched to KSUNOG branch."
+    
     git pull && echo "[+] Repository updated."
-    if [ -z "${1-}" ]; then
-        git checkout "$(git describe --abbrev=0 --tags)" && echo "[-] Checked out latest tag."
-    else
-        git checkout "$1" && echo "[-] Checked out $1." || echo "[-] Checkout default branch"
-    fi
+
     cd "$DRIVER_DIR"
     ln -sf "$(realpath --relative-to="$DRIVER_DIR" "$GKI_ROOT/KernelSU-Next/kernel")" "kernelsu" && echo "[+] Symlink created."
 
