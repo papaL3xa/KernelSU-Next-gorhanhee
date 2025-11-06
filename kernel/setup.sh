7c667b6d2c3cebce96059ce33953c720321fb1ee
papal3xa <john.scriven03@gmail.com>
2025-11-06 14:20:19 +0700

    Update setup.sh

--- kernel/setup.sh
+++ kernel/setup.sh
@@ -39,7 +39,7 @@ perform_cleanup() {
 # Sets up or update KernelSU-Next environment
 setup_kernelsu() {
     echo "[+] Setting up KernelSU-Next..."
-    test -d "$GKI_ROOT/KernelSU-Next" || git clone https://github.com/papaL3xa/KernelSU-Next-gorhanhee.git && echo "[+] Repository cloned."
+    test -d "$GKI_ROOT/KernelSU-Next" || git clone https://github.com/papaL3xa/KernelSU-Next-gorhanhee.git KernelSU-Next && echo "[+] Repository cloned."
     cd "$GKI_ROOT/KernelSU-Next"
     git stash && echo "[-] Stashed current changes."
 
